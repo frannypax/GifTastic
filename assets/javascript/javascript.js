325fd510ada48eb950619254f86545d55dbaeaac
yaw owusu <asirifiyaw@gmail.com>
2017-09-05 23:26:53 -0700

    fixed minor UI issues

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -64,18 +64,7 @@ $(document).on("click",".image", function(){
 		$(this).attr("src", $(this).data("still"));
 		$(this).attr("data-state", "still");
 	}
-})
-
-// $(document).on("click", ".image", function(){
-//     var state = $(this).attr('data-state');
-//     if ( state == 'still'){
-//         $(this).attr('src', $(this).data('animate'));
-//         $(this).attr('data-state', 'animate');
-//     }else{
-//         $(this).attr('src', $(this).data('still'));
-//         $(this).attr('data-state', 'still');
-//     }
-// });
+});
 
 
 
