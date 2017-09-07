6c2c629317f9114f5443a6233b6832a922080ae8
yaw owusu <asirifiyaw@gmail.com>
2017-09-07 08:33:35 -0700

    added in-line picture display, center-aligned rating

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,7 +2,7 @@ $( document ).ready(function(){
 
 
 //creating an array of topics
-var theme = ["Cars","Rivers", "Mountains","Dogs"];
+var theme = ["Tesla","Solar Flare", "Panda","Soccer"];
 
 function displayGifInfo(){
 
@@ -28,13 +28,12 @@ function displayGifInfo(){
 		gifDiv.addClass("gifDiv");
 
 		//rating
-		var gifRating= $("<p>").text("Rating: " + results[i].rating);
+		var gifRating= $("<p id='ratingsText'>").text("Rating: " + results[i].rating);
 		gifDiv.append(gifRating);
-
 
 		//getting image attributes
 		
-		var gifImage = $("<img>");
+		var gifImage = $("<img style='display:inline-block'>");
 		gifImage.attr("data-state", "still"); // setting a default data state
 		gifImage.attr("src",results[i].images.fixed_height_small_still.url); //the image in this url holds the default image when the theme button is clicked
 		gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); //setting attribute: data-still. this url leads to a still image (happens to be the same image above)
@@ -49,6 +48,19 @@ function displayGifInfo(){
 	})
 };
 
+
+$(document).on("click",".theme", displayGifInfo);
+
+$(document).on("click",".image", function(){
+	var state =$(this).attr("data-state");
+	if(state == "still"){
+		$(this).attr("src",$(this).data("animate"));
+		$(this).attr("data-state", "animate");
+	}else{
+		$(this).attr("src", $(this).data("still"));
+		$(this).attr("data-state", "still");
+	}
+})
 
 var displayButtons= function(){
     $("#themeDisplay").empty(); //clearing out div
@@ -77,25 +89,11 @@ displayButtons();
         	return false; // added so user cannot add a blank button
         }
        
-        // Adding theme from the textbox to our array
+        // Adding movie from the textbox to our array
         theme.push(newTheme);
         
+        // Calling renderButtons which handles the processing of our movie array
         displayButtons();
     });
-
-
-$(document).on("click",".theme", displayGifInfo); //listening for click on theme button
-
-$(document).on("click",".image", function(){ // listening for click on image
-	var state =$(this).attr("data-state");
-	if(state == "still"){
-		$(this).attr("src",$(this).data("animate"));
-		$(this).attr("data-state", "animate");
-	}else{
-		$(this).attr("src", $(this).data("still"));
-		$(this).attr("data-state", "still");
-	}
-});
-
     
 });
